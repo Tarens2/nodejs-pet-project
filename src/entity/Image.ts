5b893437d9e13ddc8428ccae432a65f8fe201d38
Taras Alechin <tarens1995@gmail.ru>
2019-09-15 22:52:08 +0300

    renames

---
+++ src/entity/Image.ts
@@ -0,0 +1,29 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+} from 'typeorm';
+import { Article } from './Article';
+
+@Entity()
+export class Image {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @ManyToOne(() => Article)
+  photo: Article;
+
+  @Column()
+  url: string;
+
+  @Column()
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
