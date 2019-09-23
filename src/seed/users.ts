3ef991fbd685589a1941a9e58dfdc537382733fa
Taras Alechin <tarens1995@gmail.ru>
2019-09-23 22:47:53 +0300

    delete old seeds

--- src/seed/users.ts
+++ src/seed/users.ts
@@ -1,20 +1,4 @@
 // insert new users for test
 import { User } from '../entity/User';
 
-export const createUsers = async (connection) => {
-  await connection.manager.save(
-    connection.manager.create(User, {
-      firstName: 'Timber',
-      lastName: 'Saw',
-      age: 27,
-    }),
-  );
-
-  await connection.manager.save(
-    connection.manager.create(User, {
-      firstName: 'Phantom',
-      lastName: 'Assassin',
-      age: 24,
-    }),
-  );
-};
+export const createUsers = async (connection) => {};
